a8ff890e84d74a872e6db1c5598c34a224e55ba0
Maximiliano Facundo Soria <107376486+Maxi-Soria@users.noreply.github.com>
2024-05-26 01:55:52 -0300

    Seccion proyectos

--- Portafolio/Portafolio/Controllers/HomeController.cs
+++ Portafolio/Portafolio/Controllers/HomeController.cs
@@ -15,10 +15,44 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
+            return View(modelo);
+        }
 
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {
+                new Proyecto
+            {
+                titulo = "Primer proyectp",
+                descripcion = "Descripcion del proy inventado",
+                link = "proyecto.com",
+                imagenUrl = "/imagenes/descarga1.jpg"
+            },
+                new Proyecto
+            {
+                titulo = "Segundo proyecto",
+                descripcion = "Descripcion del segundo proyecto",
+                link = "proyecto.com",
+                imagenUrl = "/imagenes/descarga1.jpg"
+            },
+                new Proyecto
+            {
+                titulo = "Tercer proyecto",
+                descripcion = "Descripcion del tercer proyecto",
+                link = "proyecto.com",
+                imagenUrl = "/imagenes/descarga3.jpg"
+            },
+                new Proyecto
+            {
+                titulo = "Cuarto proyecto",
+                descripcion = "Descripcion del cuarto proyecto",
+                link = "proyecto.com",
+                imagenUrl = "/imagenes/descarga4.jpg"
+            },
 
-
-            return View();
+            };
         }
 
         public IActionResult Privacy()
---
+++ Portafolio/Portafolio/Models/Proyecto.cs
@@ -0,0 +1,10 @@
+﻿namespace Portafolio.Models
+{
+    public class Proyecto
+    {
+        public String titulo { get; set; }
+        public String descripcion { get; set; }
+        public String imagenUrl { get; set; }
+        public String link { get; set; }
+    }
+}
